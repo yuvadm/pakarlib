a5697c03f789ba6cff637ee468a6d536f6238c85
Yuval Adam <_@yuv.al>
2023-10-22 12:13:12 +0200

    Add get_segments

--- pakarlib/build.py
+++ pakarlib/build.py
@@ -16,5 +16,21 @@ def get_cities():
         with open(DATA_DIR / "cities"/ f"{lang}.json", "wb") as f:
             f.write(res.content)
 
+def get_segments():
+    BASE_URL = "https://dist.meser-hadash.org.il/smart-dist/services/anonymous/polygon/id/android"
+    sess = requests.Session()
+    
+    for i in range(5000000, 5005000):
+        res = sess.get(BASE_URL, params={"id": str(i), "instance": "1544803905", "locale": "iw_IL"})
+        if "CDATA" in res.text:
+            print(res.text)
+            print(f"{i} - got CDATA error, skipping")
+        else:
+            with open(DATA_DIR / "segments" / f"{i}.json", "wb") as f:
+                cont = res.content
+                print(f"{i} - writing {len(cont)} bytes")
+                f.write(cont)
+
 if __name__ == "__main__":
     get_cities()
+    get_segments()
