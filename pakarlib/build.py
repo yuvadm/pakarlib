392e98019c9e01fefc1ecbbeaa54a923b1a04206
Yuval Adam <_@yuv.al>
2023-10-24 21:24:08 +0200

    Build districts

--- pakarlib/build.py
+++ pakarlib/build.py
@@ -23,5 +23,24 @@ def build_cities():
     with open(DATA_DIR / "build"/ "cities.json", "w") as f:
         f.write(json.dumps(list(d.values())))
 
+def build_districts():
+    print("Building districts...")
+    LANGS = ["he", "ar", "en", "ru"]
+    LANG_FIELDS = ["label", "areaname"]
+
+    d = defaultdict(dict)
+
+    for lang in LANGS:
+        with open(DATA_DIR / "districts"/ f"{lang}.json", "r") as f:
+            districts = json.load(f)
+            for district in districts:
+                for field in LANG_FIELDS:
+                    district[f"{field}_{lang}"] = district.pop(field)
+                d[district["value"]].update(district)
+    
+    with open(DATA_DIR / "build"/ "districts.json", "w") as f:
+        f.write(json.dumps(list(d.values())))
+
 if __name__ == "__main__":
     build_cities()
+    build_districts()
